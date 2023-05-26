3b9aa0174696be7e2f755abfa105a14ead029d9c
Connor Broadfoot <cgbroadfoot@gmail.com>
2023-05-26 12:05:00 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request
+from dotenv.main import load_dotenv
+import os
+load_dotenv()
+
+# OpenAI
+from langchain.tools import Tool
+from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
+from langchain.memory import ConversationBufferMemory
+from langchain import OpenAI, LLMChain
+from langchain.utilities import GoogleSearchAPIWrapper
+
+OpenAI.api_key = os.environ['OPENAI_API_KEY']
+
+
+
+search = GoogleSearchAPIWrapper()
+tools = [
+    Tool(
+        name = "Search",
+        func=search.run,
+        description="useful for when you need to answer questions about current events"
+    )
+]
+
+prefix = """Have a conversation with a human, answering the following questions as best you can. You have access to the following tools:"""
+suffix = """Begin!"
+
+{chat_history}
+Question: {input}
+{agent_scratchpad}"""
+
+prompt = ZeroShotAgent.create_prompt(
+    tools, 
+    prefix=prefix, 
+    suffix=suffix, 
+    input_variables=["input", "chat_history", "agent_scratchpad"]
+)
+memory = ConversationBufferMemory(memory_key="chat_history")
+
+llm_chain = LLMChain(llm=OpenAI(temperature=0), prompt=prompt)
+agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
+agent_chain = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory)
+
+app = Flask(__name__)
+
+@app.route('/api/new-input', methods=['POST'])
+def newInput():
+    return agent_chain.run(input=str(request.json))
+
