14088a2e82047595bcc7e73928958f1efdd0f12f
Connor Broadfoot <cgbroadfoot@gmail.com>
2023-06-10 14:45:04 -0700

    Change data structure. Modify classes to store ID differently. Update new teammate endpoint to handle different input

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+from langchain.prompts.prompt import PromptTemplate
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain import OpenAI
 from flask import Flask, request, jsonify
 from dotenv.main import load_dotenv
 import os
@@ -5,44 +9,35 @@ import random
 load_dotenv()
 
 # OpenAI
-from langchain import OpenAI
-from langchain.chains.conversation.memory import ConversationBufferMemory
-from langchain.chains import ConversationChain
-from langchain.prompts.prompt import PromptTemplate
 
 app = Flask(__name__)
 
 # Temp Data
-teammateData = [
-  {"id": 0, 
-   "name": 'Orc', 
-   "title": 'Orc Lord',
-   "character" :
-     "You are an orc and you are looking for hobits. You need to destroy the hobbits and find the ring before the hobits reach Mount Doom", 
-     "verbose": 10, 
-     "temperature": 0.7,
-   },
-  {"id": 1, "name": 'Frodo', "title": 'Hobbit', "character" : 'You are scared of everything and need to ask sam for help all the time. The fate of mankind lies in your hands'},
-  {"id": 2, "name": 'Sam', "title": 'Hobbit', "character" : 'You are obssessed with food and have a crush on Frodo. You and him used to be married as a gay couple until you decided to recently take a break. You always wonder where he is and where food is'},
-  {"id": 3, "name": 'Golum', "title": 'Creature', "character" : 'You talk like Golum from lord of the rings. You are obssessed with a ring and you have tourettes'}
-  ]
+teammateData = []
 
 conversations = []
 
+
 class Conversation:
-    def __init__(self, id):
-        self.id = id
+
+    next_id = 0
+
+    def __init__(self, teammate):
+        self.id = Conversation.next_id
+        Conversation.next_id += 1
+        self.teammateID = teammate.id
         self.messages = []
         self.llm = OpenAI(
             temperature=0,
             openai_api_key=os.getenv("OPENAI_API_KEY"),
             model_name="text-davinci-003"
         )
-        self.template = teammateData[id]['character'] + """
+
+        self.template = teammate.character + """
             Current conversation:
             {history}
             Coworker: {input}
-            """ + teammateData[id]['title'] + ':'
+            """ + teammate.title + ':'
 
         self.PROMPT = PromptTemplate(
             input_variables=["history", "input"], template=self.template
@@ -52,7 +47,8 @@ class Conversation:
             prompt=self.PROMPT,
             llm=self.llm,
             verbose=True,
-            memory=ConversationBufferMemory(ai_prefix=teammateData[id]['title'], human_prefix="Manager")
+            memory=ConversationBufferMemory(
+                ai_prefix=teammate.title, human_prefix="Manager")
         )
 
     def print_conversation(self):
@@ -61,7 +57,7 @@ class Conversation:
 
     def predict(self, input):
         return self.conversation.predict(input=str(input))
-    
+
     def newMessage(self, message):
         self.messages.append({
             'id': len(self.messages) + 1,
@@ -69,28 +65,49 @@ class Conversation:
         })
 
 
-class Teammate: 
+class Teammate:
+
+    next_id = 0
+
     def __init__(self, name, title, character, verbose, temperature):
-        self.id = random.randint(1, 10000),
-        self.name = name,
-        self.title = title,
-        self.character = character,
-        self.verbose = verbose,
+        self.id = Teammate.next_id
+        Teammate.next_id += 1
+        self.name = name
+        self.title = title
+        self.character = character
+        self.verbose = verbose
         self.temperature = temperature
-        
+
+    def to_dict(self):
+        return {
+        'id': self.id,
+        'name': self.name,
+        'title': self.title,
+        'character': self.character,
+        'verbose': self.verbose,
+        'temperature': self.temperature
+        }
+
+
+# ROUTES
 
 @app.route('/api/teammates', methods=['GET'])
 def getTeam():
-    return teammateData
+    team_dict = [teammate.to_dict() for teammate in teammateData]
+    return jsonify(team_dict)
+
 
 @app.route('/api/teammates/new', methods=['POST'])
 def newTeammate():
 
     data = request.json
-    print(data)
-     
+
     teammate = Teammate(data['name'], data['title'], data['character'], data['verbose'], data['temperature'])
+
+    teammateData.append(teammate)
+    
     return jsonify(teammate.id)
+
 
 @app.route('/api/inputs/new-input', methods=['POST'])
 def newInput():
@@ -100,20 +117,25 @@ def newInput():
     conversationID = int(data.get('conversationID'))
     input = data.get('input')
 
-    # Get conversation
     # Find conversation with matching ID
     conversation = next((convo for convo in conversations if convo.id == conversationID), None)
     if conversation:
         conversation.newMessage(input)
-        conversation.print_conversation()
         return conversation.predict(input)
     else:
         print("Conversation not found")
 
-@app.route('/api/conversations/new' , methods =['POST'])
+
+@app.route('/api/conversations/new', methods=['POST'])
 def newConversation():
-    data = request.get_json()
-    conversation_id = data.get('data')
-    conversation = Conversation(conversation_id)
+
+    # Parse Data
+    teammateID = request.json.get('data')
+
+    # # Get teammate
+    teammate = next((t for t in teammateData if t.id == teammateID), None)
+
+    # # Create Conversation
+    conversation = Conversation(teammate)
     conversations.append(conversation)
     return jsonify(conversation.id)
